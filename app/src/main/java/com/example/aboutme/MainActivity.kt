3202d83f7fe0fd06d55b5073e162d07e2584a310
Thalisson <thalissonmt@gmail.com>
2021-10-04 17:48:38 -0300

    working with Data Binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,23 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val person:Person = Person("Thalisson Tavares")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.doneButton.setOnClickListener {
-            addNickname(it)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.person = person
+        binding.apply {
+            doneButton.setOnClickListener {
+                addNickname(it)
+            }
         }
     }
 
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            person?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             doneButton.visibility = View.GONE
             nicknameEdit.visibility = View.GONE
